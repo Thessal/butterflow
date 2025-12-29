b72cc4ecf5ea03afaea5039669c08a187a71caeb
Jongkook Choi <jongkook90@hotmail.com>
2025-12-29 22:46:22 +0900

    bias test code

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -16,9 +16,11 @@ def build_random_data_pair():
     matf1 = np.random.rand(N,P)
     matf2 = matf1.copy()
     matf2[N1:] = np.random.rand(N-N1,P)
+    f = float(np.random.normal(0,1))
+    i = int(np.random.randint(-10,10))
     result[repr(Generic("Signal", Atomic("Float")))] = (matf1, matf2)
-    result[repr(Atomic("Float"))] = (float(np.random.normal(0,1)), float(np.random.normal(0,1)))
-    result[repr(Atomic("Int"))] = (int(np.random.randint(-10,10)), int(np.random.randint(-10,10)))
+    result[repr(Atomic("Float"))] = (f,f)
+    result[repr(Atomic("Int"))] = (i,i)
     return result
 
 def parse_patterns(args):
@@ -43,20 +45,28 @@ def forward_looking_test(op_name):
     args = op_info.args.fields
     patterns = parse_patterns(args)
     data = build_random_data_pair()
+    errors = []
     for pattern in patterns:
         result_A, result_B = run_pattern(op, pattern, data)
         if type(result_A) == type(result_B) == np.ndarray:
-            result_A = result_A[:N]
-            result_B = result_B[:N]
+            result_A = result_A[:N1]
+            result_B = result_B[:N1]
         if not np.array_equal(result_A, result_B, equal_nan=True):
-            print("A\n", result_A[-3:, :5], "\nB\n", result_B[-3: :5])
-            raise ValueError(f"Forward looking bias in {op_name}, {pattern}")
+            # print("A\n", result_A[-3:, :5], "\nB\n", result_B[-3: :5])
+            # ValueError(f"Forward looking bias in {op_name}, {pattern}")
+            errors.append(f"Forward looking bias in {op_name}, {pattern}")   
+    return errors
 
 def test_forward_looking():
     op_names = STD_LIB.keys()
+    errors = []
     for op_name in op_names:
         if op_name != "data":
-            forward_looking_test(op_name)
+            err = forward_looking_test(op_name)
+            errors.extend(err)
+    if errors:
+        print("\n".join(errors))
+        assert not errors
 
 if __name__=="__main__":
     pytest.main()
